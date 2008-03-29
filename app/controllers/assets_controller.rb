12e9d61ed55e5dd343494352b2c1c0a9c535cc7c
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-03-29 16:26:26 -0400

    added assets views

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,8 +1,34 @@
 class AssetsController < ApplicationController
+  
+  before_filter :find_project
 
   make_resourceful do
     actions :all
-    belongs_to :project
+
+    response_for :create do 
+      redirect_to project_assets_path(@project)
+    end
+
+    response_for :update do 
+      redirect_to project_assets_path(@project)
+    end
+
+    response_for :destroy do 
+      redirect_to project_assets_path(@project)
+    end
+
+
+  end
+  
+  
+private
+  
+  def current_objects
+    @current_object ||= @project.asset_categories.map(&:assets)
+  end
+
+  def find_project
+    @project = Project.find params[:project_id]
   end
 
 end
