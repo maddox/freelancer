78bb35682cb31695066dfd195c9c844e5a0847a7
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-03-29 13:13:47 -0400

    added  project spec and its specs

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ActiveRecord::Base
+  validates_presence_of :title
+  
+  belongs_to :client
 end
---
+++ spec/models/project_spec.rb
@@ -0,0 +1,38 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Project do
+  fixtures :clients, :projects
+
+  describe "when being created" do
+
+    before(:each) do
+      @project = Project.new( :title => 'Million Dollar Social Network',
+                              :description => '',
+                              :repository => '',
+                              :client_id => clients(:microsoft))
+    end
+
+    it "should require a title" do
+      @project.title = nil
+      @project.save
+      @project.should_not be_valid
+    end
+
+  end
+
+  describe "with associations" do
+
+    it "should belong to a client" do
+      Project.reflect_on_association(:client).should_not be(nil)
+    end
+
+    it "should belong to a client officially" do
+      @project = projects(:big_project)
+      @project.client.should == clients(:microsoft)
+    end
+
+  end
+
+
+
+end
