7bd1d4aa93844905784ec9040ab83aa332a20e8a
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-03-29 13:04:40 -0400

    added  project spec

--- db/schema.rb
+++ db/schema.rb
@@ -9,10 +9,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 1) do
+ActiveRecord::Schema.define(:version => 2) do
 
   create_table "clients", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", :force => true do |t|
+    t.integer  "client_id"
+    t.string   "title"
+    t.text     "description"
+    t.string   "repository"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
