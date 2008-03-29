d5000fc168030a0341f3fbe381aad34ec264b661
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-03-29 12:25:43 -0400

    initial import

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'd4b62eac63d117d61902c6f9debc19fd'
+end
