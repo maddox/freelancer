d423ee7eb08920369fe9ed6da46a857619d441c4
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-03-29 13:56:20 -0400

    added attachment to asset model

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,4 +1,11 @@
 class Asset < ActiveRecord::Base
   validates_presence_of :title
   belongs_to :category, :class_name => "AssetCategory", :foreign_key => "asset_category_id"
+
+
+  has_attached_file :attachment, 
+                    :styles => { :thumb => "300x300>",
+                                 :square  => "75x75#" }
+
+
 end
