db0dd189ae918fb9cb5a6228a46e6bf57986ac25
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-03-29 12:59:22 -0400

    required name

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,4 @@
 class Client < ActiveRecord::Base
+
+  validates_presence_of :name
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,11 +1,20 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Client do
-  before(:each) do
-    @client = Client.new
+
+  describe "without proper attributes" do
+
+    before(:each) do
+      @client = Client.new(:name => 'Happy Company')
+    end
+
+    it "should require a name" do
+      @client.name = nil
+      @client.save
+      @client.should_not be_valid
+    end
+
   end
 
-  it "should be valid" do
-    @client.should be_valid
-  end
+
 end
