c37b2638d6d76637b7f341a74d300bd0fc6276d3
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-03-29 14:24:45 -0400

    nested assets into projects

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -2,6 +2,7 @@ class AssetsController < ApplicationController
 
   make_resourceful do
     actions :all
+    belongs_to :project
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.resources :clients, :has_many => :projects
-  map.resources :projects
-  map.resources :assets
+  map.resources :projects, :has_many => :assets
 
 
 end
